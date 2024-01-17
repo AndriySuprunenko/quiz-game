4e6a1c3ecfb1862b82e4ef93bd564a4d5643772e
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-01-17 15:28:46 +0200

    add styles and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from 'react';
+import jsonData from './data.json';
+import './App.css';
+
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(jsonData);
+  }, []);
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="wrapper">
+        {data.map((item) => (
+          <>
+            <h1 className="title" key={item.id}>
+              <span>{item.id}.</span>
+              {item.question}
+            </h1>
+            <div className="answers">
+              {item.answers.map((answer) => {
+                return (
+                  <button className="button" key={answer.id}>
+                    <span>{answer.id}.</span>
+                    {answer.title}
+                  </button>
+                );
+              })}
+            </div>
+          </>
+        ))}
+      </div>
     </>
   );
 };
