bf5968091059c43c141a6decf21429a008eff98d
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-01-19 19:13:26 +0200

    add styles and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from 'react';
-import jsonData from './data.json';
-import './App.css';
+import Data from './components/Data';
 
 const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(jsonData);
-  }, []);
-
   return (
     <>
-      <div className="wrapper">
-        {data.map((item) => (
-          <>
-            <h1 className="title" key={item.id}>
-              <span>{item.id}.</span>
-              {item.question}
-            </h1>
-            <div className="answers">
-              {item.answers.map((answer) => {
-                return (
-                  <button className="button" key={answer.id}>
-                    <span>{answer.id}.</span>
-                    {answer.title}
-                  </button>
-                );
-              })}
-            </div>
-          </>
-        ))}
-      </div>
+      <Data />
     </>
   );
 };
